b5d6c02cd32c1c4ecad4e985c6eab94644088f26
Yasura Lakshitha <lakshitha200087@gmail.com>
2024-11-04 22:02:04 +0530

    create base class Book

---
+++ lib/model/Book.dart
@@ -0,0 +1,40 @@
+import 'dart:core';
+
+class Book {
+
+  String _title;
+  String _author;
+  String _isbn;
+  bool _isUnavailable;
+
+  Book(this._title, this._author, this._isbn, this._isUnavailable);
+
+  bool get isUnavailable => _isUnavailable;
+
+  set isUnavailable(bool value) {
+    _isUnavailable = value;
+  }
+
+  String get isbn => _isbn;
+
+  set isbn(String value) {
+    _isbn = value;
+  }
+
+  String get author => _author;
+
+  set author(String value) {
+    _author = value;
+  }
+
+  String get title => _title;
+
+  set title(String value) {
+    _title = value;
+  }
+
+  @override
+  String toString() {
+    return 'Book{_title: $_title, _author: $_author, _isbn: $_isbn, _isUnavailable: $_isUnavailable}';
+  }
+}
