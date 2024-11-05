7761f7c983b857d170a3f0960673780d8d0438c9
Yasura Lakshitha <lakshitha200087@gmail.com>
2024-11-05 06:13:25 +0530

    implement create method

---
+++ lib/service/BookService.dart
@@ -0,0 +1,11 @@
+import 'package:library_management_system/repository/BookRepository.dart';
+
+import '../model/Book.dart';
+
+class BookService{
+  BookRepository repository = new BookRepository();
+
+  Book saveBook(Book book){
+    return repository.save(book);
+  }
+}
